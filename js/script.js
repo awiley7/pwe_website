c8a6567e3d4f9c05b531ba6dcc895c602ee03761
Cris Pineda <cspineda559@gmail.com>
2024-02-27 00:43:05 +0100

    remove sticky nav

--- js/script.js
+++ js/script.js
@@ -24,30 +24,30 @@ btnNavEl.addEventListener("click", function () {
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+// const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    if (!ent.isIntersecting) {
-      console.log(ent);
-      document.body.classList.add("sticky");
-    }
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     if (!ent.isIntersecting) {
+//       console.log(ent);
+//       document.body.classList.add("sticky");
+//     }
 
-    if (ent.isIntersecting) {
-      console.log(ent);
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
+//     if (ent.isIntersecting) {
+//       console.log(ent);
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
 
-obs.observe(sectionHeroEl);
+// obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling nav (doesn't work for old safari should check)
